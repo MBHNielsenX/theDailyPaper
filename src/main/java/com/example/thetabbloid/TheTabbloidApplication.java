9c466a08656b10582cd8b69856beeb96107a44c1
MBHNielsenX <mads.nielsen12@live.dk>
2022-02-23 19:57:46 +0100

    first

---
+++ src/main/java/com/example/thetabbloid/TheTabbloidApplication.java
@@ -0,0 +1,13 @@
+package com.example.thetabbloid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTabbloidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheTabbloidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thetabbloid/TheTabbloidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thetabbloid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheTabbloidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
